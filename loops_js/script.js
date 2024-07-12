a826b775cf78b37b02f72daa3c99065f6065b32c
Chaitanya <chaitanyayadwad8@gmail.com>
2024-07-12 15:24:38 +0530

    loop in js

---
+++ loops_js/script.js
@@ -0,0 +1,31 @@
+// // console.log('chaitanya');
+// //for loop
+// for(let i=0;i<10;i++){
+//     console.log('chaitanya',i);
+// }
+
+//while loop
+// let step=0;
+// while(step<5){
+//     console.log('steo',step);
+//     step++;
+// }
+// let step=0;
+// do{
+//     console.log('step',step);
+//     step+=1;
+// }while(step < 5)
+
+let step=0;
+while(step<5){
+    step++;
+    if(step==2){
+        continue;
+    }
+    console.log(step);
+}
+
+
+    
+    
+
