9bb4dc691a4223baf642425391007ad69796d1ce
Chaitanya <chaitanyayadwad8@gmail.com>
2024-07-12 12:14:40 +0530

    some basic topics

---
+++ basic/script.js
@@ -0,0 +1,10 @@
+let c=80;
+let d= 40;
+console.log('c-d is',c-d);
+let age;//declaration
+age=25;//intialization
+const name="chitany"; 
+console.log('age: ',age);
+age=44;                               
+console.log('new age: ',age);
+console.log('name',name);
