8f5e6f0375bd839a7f651fdef4215cebc8767f35
Chaitanya <chaitanyayadwad8@gmail.com>
2024-07-12 12:16:22 +0530

    datatype and operator in js

---
+++ datatype_js/script.js
@@ -0,0 +1,17 @@
+let a=23;
+// a=false;
+// a="my name is chait"
+// a=a+1;
+// console.log(a);
+
+// a="23"
+// a=a+1;
+// console.log(a);
+ a=String(a);
+ console.log('the value of a is',a);         
+ console.log('the type of a is',typeof(a));                               
+
+ let b=null;
+ b=String(b);
+ console.log('the value of b is',b);
+ console.log('type of b is',typeof(b));
---
+++ operator_js/script.js
@@ -0,0 +1,45 @@
+const x=4;
+const y=5;
+const result=x+y;
+console.log(result);
+
+//modulo operator % - it will give remainder
+const n1=2;
+const n2=3;
+console.log(n1%n2);
+
+//increment & decrement operator
+// n1++;
+// console.log(n1);
+let d;
+d=45;
+d++;//increment operator
+console.log(d);
+d-- //decrement operator
+console.log(d);
+
+//exponentiation (power) operator
+console.log(n1 ** n2);
+ 
+
+// assignment operator
+let no=12;
+no+=4;//no=no+4
+console.log(no);
+
+//comparison operator- it is used to compare two operand
+let ro=45;
+let ko='45';
+let res = ro==ko;
+console.log(res);
+
+//strict equal to (===)
+ro=56;
+ko=35;
+console.log(ro===ko);
+
+//logical operator
+console.log(ro>45 && ko<40);
+//logical not operator
+ro=true;
+console.log(!ro);
